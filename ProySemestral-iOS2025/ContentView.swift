9dbc4588e666032ab1c1f9a4e9e2fcd9b0b143c7
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-17 18:34:46 -0600

    Initial Commit

---
+++ ProySemestral-iOS2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProySemestral-iOS2025
+//
+//  Created by WIN603 on 17/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProySemestral-iOS2025/ProySemestral_iOS2025App.swift
@@ -0,0 +1,17 @@
+//
+//  ProySemestral_iOS2025App.swift
+//  ProySemestral-iOS2025
+//
+//  Created by WIN603 on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ProySemestral_iOS2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
