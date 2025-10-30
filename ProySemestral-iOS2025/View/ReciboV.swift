21cc89011275bf12a3668160bfc43bac5da46fc3
Fabiola <fabiola.juarez>
2025-10-29 18:45:05 -0600

    .

---
+++ ProySemestral-iOS2025/View/ReciboV.swift
@@ -0,0 +1,18 @@
+//
+//  ReciboV.swift
+//  ProySemestral-iOS2025
+//
+//  Created by win603 on 29/10/25.
+//
+
+import SwiftUI
+
+struct ReciboV: View {
+    var body: some View {
+        Text(":3")
+    }
+}
+
+#Preview {
+    ReciboV()
+}
